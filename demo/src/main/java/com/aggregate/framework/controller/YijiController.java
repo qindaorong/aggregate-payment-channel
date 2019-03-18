f30ea4b99aae5f4161d55c49673c5c708b7b95b9
523506791 <523506791@qq.com>
2019-03-18 18:50:52 +0800

    response is update

--- demo/src/main/java/com/aggregate/framework/controller/YijiController.java
+++ demo/src/main/java/com/aggregate/framework/controller/YijiController.java
@@ -31,7 +31,7 @@ public class YijiController {
      */
     @PostMapping(value = "/verifyBankCard")
     @ApiOperation(value="验卡接口使用时,可进行三要素验卡、四要素验卡或四要素加短信验卡",httpMethod = "POST", notes = "VerifyBankCardDto  请求实体具体参数以swagger文档说明为参考")
-    public CommonResponse verifyBankCard(@RequestBody final @Valid VerifyBankCardDto verifyBankCardDto){
+    public ResponseResult verifyBankCard(@RequestBody final @Valid VerifyBankCardDto verifyBankCardDto,BindingResult bindingResult){
         /*VerifyBankCardDto verifyBankCardDto =
                 VerifyBankCardDto.builder()
                         .bankCardNo("6226095711393038")
@@ -40,9 +40,18 @@ public class YijiController {
                         .mobileNo("18317146596")
                         .name("马涛")
                         .outOrderNo("1234567891").build();*/
-        AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(verifyBankCardDto).enums(PayChannelEnums.YIJI).build();
-        return  yijiService.verifyBankCard(aggregateRequestDto);
-        //log.debug("[aggregayrPayService][verifyBankCard] value is {}",isPass);
+        if(bindingResult.hasErrors()){
+            CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+            return  ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+        }else{
+            AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(verifyBankCardDto).enums(PayChannelEnums.YIJI).build();
+            CommonResponse commonResponse = yijiService.verifyBankCard(aggregateRequestDto);
+            if(commonResponse.getFlag()){
+                return ResponseResult.success(commonResponse);
+            }else{
+                return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+            }
+        }
     }
 
     /**
@@ -50,14 +59,22 @@ public class YijiController {
      */
     @PostMapping(value = "/verifyBankCardQuery")
     @ApiOperation(value="验卡查询接口",httpMethod = "POST", notes = "VerifyBankCardQueryDto  请求实体具体参数以swagger文档说明为参考")
-    public CommonResponse verifyBankCardQuery(@RequestBody final @Valid VerifyBankCardQueryDto verifyBankCardQueryDto){
+    public ResponseResult verifyBankCardQuery(@RequestBody final @Valid VerifyBankCardQueryDto verifyBankCardQueryDto,BindingResult bindingResult){
      /*   VerifyBankCardQueryDto verifyBankCardQueryDto =
                 VerifyBankCardQueryDto.builder()
                         .outOrderNo("1234567891").build();*/
-
-        AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(verifyBankCardQueryDto).enums(PayChannelEnums.YIJI).build();
-        return yijiService.verifyBankCardQuery(aggregateRequestDto);
-        //log.debug("[aggregayrPayService][verifyBankCard] value is {}",isPass);
+     if(bindingResult.hasErrors()){
+         CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+         return  ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+     }else{
+         AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(verifyBankCardQueryDto).enums(PayChannelEnums.YIJI).build();
+         CommonResponse commonResponse = yijiService.verifyBankCardQuery(aggregateRequestDto);
+         if(commonResponse.getFlag()){
+             return ResponseResult.success(commonResponse);
+         }else{
+             return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+         }
+     }
     }
 
 
@@ -67,7 +84,7 @@ public class YijiController {
      */
     @ApiOperation(value="放款接口",httpMethod = "POST", notes = "LoanDto  请求实体具体参数以swagger文档说明为参考")
     @PostMapping(value = "/loan")
-    public CommonResponse loan(@RequestBody final @Valid LoanDto loanDto){
+    public ResponseResult loan(@RequestBody final @Valid LoanDto loanDto,BindingResult bindingResult){
        /* BigDecimal bigDecimal = new BigDecimal(1);
         Date date = new Date();
         LoanDto loanDto =
@@ -81,8 +98,18 @@ public class YijiController {
                         .bankCode("CMB")
                         .purpose("代发工资")
                         .build();*/
-        AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(loanDto).enums(PayChannelEnums.YIJI).build();
-        return yijiService.loan(aggregateRequestDto);
+       if(bindingResult.hasErrors()){
+           CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+           return ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+       }else{
+           AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(loanDto).enums(PayChannelEnums.YIJI).build();
+           CommonResponse commonResponse = yijiService.loan(aggregateRequestDto);
+           if(commonResponse.getFlag()){
+               return ResponseResult.success(commonResponse);
+           }else{
+               return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+           }
+       }
     }
 
 
@@ -94,7 +121,7 @@ public class YijiController {
      */
     @ApiOperation(value=" 添加银行卡（签约）",httpMethod = "POST", notes = "ApplyCardDto  请求实体具体参数以swagger文档说明为参考")
     @PostMapping(value = "/addApplyCard")
-    public CommonResponse addApplyCard(@RequestBody final @Valid ApplyCardDto applyCardDto){
+    public ResponseResult addApplyCard(@RequestBody final @Valid ApplyCardDto applyCardDto,BindingResult bindingResult){
        /* ApplyCardDto applyCardDto =
                 ApplyCardDto.builder()
                         .enums(ApplyChannelEnums.unionpay)
@@ -102,10 +129,21 @@ public class YijiController {
                         .signName("马涛")
                         .signID("610429199009085178")
                         .signMobile("18317146596")
-                        .merchOrderNo("12345678901234567890")
+                        .merchOrderNo("12345678901234567891")
                         .build();*/
-        AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(applyCardDto).enums(PayChannelEnums.YIJI).build();
-        return yijiService.addApplyCard(aggregateRequestDto);
+
+        if(bindingResult.hasErrors()){
+            CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+            return ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+        }else{
+            AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(applyCardDto).enums(PayChannelEnums.YIJI).build();
+            CommonResponse commonResponse = yijiService.addApplyCard(aggregateRequestDto);
+            if(commonResponse.getFlag()){
+                return ResponseResult.success(commonResponse);
+            }else{
+                return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+            }
+        }
     }
 
 
@@ -130,7 +168,11 @@ public class YijiController {
         }else{
             AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(cardAddConfirmDto).enums(PayChannelEnums.YIJI).build();
             CommonResponse response = yijiService.cardAddConfirm(aggregateRequestDto);
-            return ResponseResult.success(response);
+            if(response.getFlag()){
+                return ResponseResult.success(response);
+            }else{
+                return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,response.getResultMessage(),response);
+            }
         }
     }
 
@@ -140,17 +182,27 @@ public class YijiController {
      *  删除银行卡（解约）
      *  statues : hold on
      */
-    @ApiOperation(value="删除银行卡（解约）",httpMethod = "POST", notes = "DeleteCardDto  请求实体具体参数以swagger文档说明为参考")
+    @ApiOperation(value="删除银行卡（解约",httpMethod = "POST", notes = "DeleteCardDto  请求实体具体参数以swagger文档说明为参考")
     @PostMapping(value = "/cardDelete")
-    public CommonResponse cardDelete(@RequestBody final @Valid DeleteCardDto deleteCardDto){
+    public ResponseResult cardDelete(@RequestBody final @Valid DeleteCardDto deleteCardDto,BindingResult bindingResult){
       /*  DeleteCardDto deleteCardDto =
                 DeleteCardDto.builder()
                         // 申请添加银行卡时，由易极付返回
                         .signNo("000q01o01j1b6qzjwk00")
                         .merchOrderNo("12345678901234567890")
                         .build();*/
-        AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(deleteCardDto).enums(PayChannelEnums.YIJI).build();
-        return yijiService.cardDelete(aggregateRequestDto);
+        if(bindingResult.hasErrors()){
+            CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+            return ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+        }else{
+            AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(deleteCardDto).enums(PayChannelEnums.YIJI).build();
+            CommonResponse commonResponse = yijiService.cardDelete(aggregateRequestDto);
+            if(commonResponse.getFlag()){
+                return ResponseResult.success(commonResponse);
+            }else{
+                return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+            }
+        }
     }
 
 
@@ -159,7 +211,7 @@ public class YijiController {
      */
     @ApiOperation(value="扣款",httpMethod = "POST", notes = "EntrustPayDto  请求实体具体参数以swagger文档说明为参考")
     @PostMapping(value = "/payEntrustPay")
-    public CommonResponse payEntrustPay(@RequestBody final @Valid EntrustPayDto entrustPayDto){
+    public ResponseResult payEntrustPay(@RequestBody final @Valid EntrustPayDto entrustPayDto,BindingResult bindingResult){
        /* BigDecimal bigDecimal = new BigDecimal(1);
         EntrustPayDto entrustPayDto =
                 EntrustPayDto.builder()
@@ -170,7 +222,16 @@ public class YijiController {
                         // 申请添加银行卡时，由易极付返回
                         .signNo("")
                         .build();*/
+        if(bindingResult.hasErrors()){
+            CodeMessage codeMessage = ExceptionAdvice.handleMethodArgumentNotValidException(bindingResult);
+            return  ResponseResult.fail(codeMessage.getCode(),codeMessage.getMessage());
+        }
         AggregateRequestDto aggregateRequestDto = AggregateRequestDto.builder().t(entrustPayDto).enums(PayChannelEnums.YIJI).build();
-        return yijiService.payEntrustPay(aggregateRequestDto);
+        CommonResponse commonResponse = yijiService.payEntrustPay(aggregateRequestDto);
+        if(commonResponse.getFlag()){
+            return ResponseResult.success(commonResponse);
+        }else{
+            return ResponseResult.fail(ExceptionAdvice.EXCEPTCODE,commonResponse.getResultMessage(),commonResponse);
+        }
     }
 }
