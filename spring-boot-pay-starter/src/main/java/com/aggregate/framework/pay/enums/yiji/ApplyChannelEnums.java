7a8704995e898b65ad67eb78df23cc152f15ede9
qinrong726 <qindaorong@126.com>
2019-03-18 17:39:11 +0800

    fix bugs

--- spring-boot-pay-starter/src/main/java/com/aggregate/framework/pay/enums/yiji/ApplyChannelEnums.java
+++ spring-boot-pay-starter/src/main/java/com/aggregate/framework/pay/enums/yiji/ApplyChannelEnums.java
@@ -1,10 +1,14 @@
 package com.aggregate.framework.pay.enums.yiji;
 
+import com.alibaba.fastjson.annotation.JSONType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JSONType(serializeEnumAsJavaBean = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ApplyChannelEnums {
 
 	unionpay("card.add.apply.unionpay"),
